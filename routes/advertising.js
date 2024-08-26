1b100b99df1cea3a5b924e1ac38c36cfeba7e61b
abdusaep <mohammad@peko.one>
2024-08-26 19:58:12 +0530

    fix: about page and header, footer updation in advertising section and color updation

--- routes/advertising.js
+++ routes/advertising.js
@@ -1,11 +1,13 @@
 const express= require('express');
 const { home } = require('../controller/advertising');
 const { contact } = require('../controller/advertising/contact');
+const { about } = require('../controller/advertising/about');
 
 const router = express.Router();
 
 router.get('/',home)
 router.get('/contact',contact)
+router.get('/about',about)
 
 
 module.exports=router
